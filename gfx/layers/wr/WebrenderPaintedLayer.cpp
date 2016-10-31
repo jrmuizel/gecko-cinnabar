f4fa0e09835924b2f51281875bdc48cee1b262e1
Jeff Muizelaar <jmuizelaar@mozilla.com>
2016-10-31 15:14:15 -0400

    Add some mask layer support

--- gfx/layers/wr/WebrenderPaintedLayer.cpp
+++ gfx/layers/wr/WebrenderPaintedLayer.cpp
@@ -66,8 +66,39 @@ WebRenderPaintedLayer::RenderLayer(wrstate* aWRState)
   } else {
       clip = rect;
   }
+
+  WRImageMask* mask = nullptr;
+  WRImageMask imageMask;
+  Layer* maskLayer = GetMaskLayer();
+
+  if (maskLayer) {
+    RefPtr<SourceSurface> surface = WebRenderLayer::ToWebRenderLayer(maskLayer)->GetAsSourceSurface();
+    if (surface) {
+      Matrix transform;
+      Matrix4x4 effectiveTransform = maskLayer->GetEffectiveTransform();
+      DebugOnly<bool> maskIs2D = effectiveTransform.CanDraw2D(&transform);
+      NS_ASSERTION(maskIs2D, "How did we end up with a 3D transform here?!");
+      //transform.PostTranslate(-aDeviceOffset.x, -aDeviceOffset.y);
+      {
+          RefPtr<DataSourceSurface> dataSurface = surface->GetDataSurface();
+          DataSourceSurface::ScopedMap map(dataSurface, DataSourceSurface::MapType::READ);
+          gfx::IntSize size = surface->GetSize();
+          MOZ_RELEASE_ASSERT(surface->GetFormat() == SurfaceFormat::A8, "bad format");
+          MOZ_RELEASE_ASSERT(size.width == map.GetStride(), "bad stride");
+          WRImageKey maskKey = wr_add_image(aWRState, size.width, size.height, map.GetStride(),
+                                            A8, map.GetData(), size.height * map.GetStride());
+
+          imageMask.image = maskKey;
+          imageMask.rect = toWrRect(rect);
+          imageMask.repeat = false;
+          Manager()->AddImageKeyForDiscard(maskKey);
+          mask = &imageMask;
+      }
+    }
+  }
+
   if (gfxPrefs::LayersDump()) printf_stderr("PaintedLayer %p using rect:%s clip:%s\n", this, Stringify(rect).c_str(), Stringify(clip).c_str());
-  wr_dp_push_image(aWRState, toWrRect(rect), toWrRect(clip), NULL, key);
+  wr_dp_push_image(aWRState, toWrRect(rect), toWrRect(clip), mask, key);
   Manager()->AddImageKeyForDiscard(key);
 
   Rect relBounds = TransformedVisibleBoundsRelativeToParent();
