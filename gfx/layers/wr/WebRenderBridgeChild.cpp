e44ebbfd1eb848cfccf1dce6ce4b04800433537b
Kartikaya Gupta <kgupta@mozilla.com>
2016-11-03 11:31:19 -0400

    Have the WRLayerManager hold a ref to a WRBridge pair.
    This fakes out the IPDL boundary just so we can make stuff from
    the WebRenderLayerManager over to the "compositor side" and make
    the split a bit easier. The fake IPDL boundary will be replaced by
    a real one in a future patch.

--- gfx/layers/wr/WebRenderBridgeChild.cpp
+++ gfx/layers/wr/WebRenderBridgeChild.cpp
@@ -6,11 +6,108 @@
 
 #include "mozilla/layers/WebRenderBridgeChild.h"
 
+#include "mozilla/layers/WebRenderBridgeParent.h"
+
 namespace mozilla {
 namespace layers {
 
 WebRenderBridgeChild::WebRenderBridgeChild(const uint64_t& aPipelineId)
+  : mWRParent(new WebRenderBridgeParent(aPipelineId))
 {
+}
+
+void
+WebRenderBridgeChild::CallCreate(const uint32_t& aWidth,
+                                 const uint32_t& aHeight)
+{
+  mWRParent->RecvCreate(aWidth, aHeight);
+}
+
+void
+WebRenderBridgeChild::CallDestroy()
+{
+  mWRParent->RecvDestroy();
+}
+
+void
+WebRenderBridgeChild::CallAddImage(const uint32_t& aWidth,
+                                   const uint32_t& aHeight,
+                                   const uint32_t& aStride,
+                                   const WRImageFormat& aFormat,
+                                   const ByteBuffer& aBuffer,
+                                   WRImageKey* aOutImageKey)
+{
+  mWRParent->RecvAddImage(aWidth, aHeight, aStride, aFormat, aBuffer, aOutImageKey);
+}
+
+void
+WebRenderBridgeChild::CallUpdateImage(const WRImageKey& aImageKey,
+                                      const uint32_t& aWidth,
+                                      const uint32_t& aHeight,
+                                      const WRImageFormat& aFormat,
+                                      const ByteBuffer& aBuffer)
+{
+  mWRParent->RecvUpdateImage(aImageKey, aWidth, aHeight, aFormat, aBuffer);
+}
+
+void
+WebRenderBridgeChild::CallDeleteImage(const WRImageKey& aImageKey)
+{
+  mWRParent->RecvDeleteImage(aImageKey);
+}
+
+void
+WebRenderBridgeChild::CallPushDLBuilder()
+{
+  mWRParent->RecvPushDLBuilder();
+}
+
+void
+WebRenderBridgeChild::CallPopDLBuilder(const WRRect& aBounds,
+                                       const WRRect& aOverflow,
+                                       const gfx::Matrix4x4& aMatrix,
+                                       const uint64_t& aScrollId)
+{
+  mWRParent->RecvPopDLBuilder(aBounds, aOverflow, aMatrix, aScrollId);
+}
+
+void
+WebRenderBridgeChild::CallDPBegin(const uint32_t& aWidth,
+                                  const uint32_t& aHeight)
+{
+  mWRParent->RecvDPBegin(aWidth, aHeight);
+}
+
+void
+WebRenderBridgeChild::CallDPEnd()
+{
+  mWRParent->RecvDPEnd();
+}
+
+void
+WebRenderBridgeChild::CallDPPushRect(const WRRect& aBounds,
+                                     const WRRect& aClip,
+                                     const float& r, const float& g,
+                                     const float& b, const float& a)
+{
+  mWRParent->RecvDPPushRect(aBounds, aClip, r, g, b, a);
+}
+
+void
+WebRenderBridgeChild::CallDPPushImage(const WRRect& aBounds,
+                                      const WRRect& aClip,
+                                      const Maybe<WRImageMask>& aMask,
+                                      const WRImageKey& aKey)
+{
+  mWRParent->RecvDPPushImage(aBounds, aClip, aMask, aKey);
+}
+
+void
+WebRenderBridgeChild::CallDPPushIframe(const WRRect& aBounds,
+                                       const WRRect& aClip,
+                                       const uint64_t& aLayersId)
+{
+  mWRParent->RecvDPPushIframe(aBounds, aClip, aLayersId);
 }
 
 } // namespace layers
